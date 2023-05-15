5155e744437899e336f3318bb91187ee6abe219d
Scott Wu <sw@scottwu.ca>
2023-05-15 02:38:25 -0700

    progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,42 @@
 <script>
-	let url = '';
+	import { onMount } from 'svelte';
 
-	const getUrl = async () => {
-		const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+	let bookmarks;
+	const appName = 'oneTabSuper';
+	let worms = [];
+	let search = '';
+	const secretFolder = `${appName} (Do Not Touch!)`;
 
-		if (tab.url !== null) url = tab.url;
+	const isFolder = (node) => node.url === undefined;
+
+	const addBookmark = async () => {
+		const test1 = await bookmarks.create({ title: 'home town', url: 'https://google2.com' });
 	};
+
+	const seeTree = async () => (worms = await bookmarks.getTree());
+
+	const findBookmark = async () => {
+		const list = (await bookmarks.search({ title: search })).map((node) => {
+			node.isFolder = isFolder(node);
+			return node;
+		});
+		worms = list;
+	};
+
+	onMount(async () => {
+		({ bookmarks } = chrome);
+
+		worms = (await bookmarks.search({ title: secretFolder })).map((node) => {
+			node.isFolder = isFolder(node);
+			return node;
+		});
+	});
 </script>
 
-<button on:click={getUrl}>reveal current url</button>
-{#if url}
-	<div>
-		The current url is: {url}
-	</div>
-{/if}
+<button on:click={seeTree}>seeTree</button>
+<button on:click={addBookmark}>add bookmark</button>
+<input type="text" bind:value={search} />
+<button on:click={findBookmark}>find bookmark</button>
+{#each worms as bookmark, i (i)}
+	<pre>{JSON.stringify(bookmark, null, 2)}</pre>
+{/each}
--- static/background.js
+++ static/background.js
@@ -1,5 +1,36 @@
-chrome.action.onClicked.addListener(() => {
+const { tabs, runtime, action, contextMenus, bookmarks } = chrome;
+
+const openTabList = () => {
 	const fileName = 'index.html';
-	const url = chrome.runtime.getURL(fileName);
-	chrome.tabs.create({ url });
+	const url = runtime.getURL(fileName);
+	tabs.create({ url });
+};
+
+const saveCurrentTab = async () => {
+	const { title, url, id } = (await tabs.query({ active: true, currentWindow: true }))[0];
+
+	const test1 = await bookmarks.create({ title, url });
+	tabs.remove(id);
+};
+
+// left click toolbar icon
+action.onClicked.addListener(saveCurrentTab);
+
+// create context menu for right click toolbar icon
+contextMenus.create({
+	title: 'Display Tab List',
+	contexts: ['action'],
+	id: 'openTabList'
+});
+
+// listen for right clicks for toolbar icon
+contextMenus.onClicked.addListener((e) => {
+	switch (e.menuItemId) {
+		case 'openTabList':
+			openTabList();
+			break;
+
+		default:
+			break;
+	}
 });
