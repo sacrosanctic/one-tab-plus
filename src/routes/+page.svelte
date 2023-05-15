b69df7ce32f6a3ca7e5740483688bc4ac97e134e
Scott Wu <sw@scottwu.ca>
2023-05-15 04:38:43 -0700

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { onMount } from 'svelte'
 
-	let bookmarks
+	let bookmarks, runtime
 	const appName = 'oneTabSuper'
+	const secretFolder = `${appName} (Do Not Touch!)`
 	let worms = []
 	let search = ''
-	const secretFolder = `${appName} (Do Not Touch!)`
 
 	const isFolder = (node) => node.url === undefined
 
@@ -28,29 +28,55 @@
 	}
 	const removeBookmark = (id) => {
 		bookmarks.remove(id)
+		loadBookmarks()
 	}
-
-	onMount(async () => {
-		;({ bookmarks } = chrome)
-
+	const getUrl = (u) => {
+		const url = new URL(chrome.runtime.getURL('/_favicon/'))
+		url.searchParams.set('pageUrl', u)
+		url.searchParams.set('size', '24')
+		return url.toString()
+	}
+	const loadBookmarks = async () => {
 		const nodes = await bookmarks.search({ title: secretFolder })
-
 		if (nodes.length === 0) {
 			//create folder
 			return
 		}
-
 		const myFolder = nodes[0]
 		if (!isFolder(myFolder)) return
 		worms = await bookmarks.getChildren(myFolder.id)
+	}
+
+	onMount(async () => {
+		;({ bookmarks, runtime } = chrome)
+
+		loadBookmarks()
+		bookmarks.onCreated.addListener(loadBookmarks)
+		bookmarks.onChanged.addListener(loadBookmarks)
+		bookmarks.onChildrenReordered.addListener(loadBookmarks)
+		bookmarks.onMoved.addListener(loadBookmarks)
+		bookmarks.onRemoved.addListener(loadBookmarks)
 	})
 </script>
 
-<button on:click={seeTree}>seeTree</button>
-<button on:click={addBookmark}>add bookmark</button>
-<input type="text" bind:value={search} />
-<button on:click={findBookmark}>find bookmark</button>
-{#each worms as { id, title, url }, i (i)}
-	<!-- <pre>{JSON.stringify(bookmark, null, 2)}</pre> -->
-	<a href={url} on:click={removeBookmark(id)} target="_blank">{title}</a>
-{/each}
+<!-- <div class="form-control">
+	<button class="btn" on:click={seeTree}>seeTree</button>
+	<button class="btn" on:click={addBookmark}>add bookmark</button>
+	<div class="input-group">
+		<input class="input input-bordered" type="text" bind:value={search} />
+		<button class="btn" on:click={findBookmark}>find bookmark</button>
+	</div>
+</div> -->
+<main class="m-2">
+	<h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Scott's Bookmark Thing</h2>
+	<ul class="pl-4 max-w-md space-y-1 text-gray-500 dark:text-gray-400 text-lg">
+		{#each worms as bookmark, i (i)}
+			<!-- <pre>{JSON.stringify(bookmark, null, 2)}</pre> -->
+			<li>
+				<a href={bookmark.url} on:click={removeBookmark(bookmark.id)} target="_blank"
+					><img class="inline" src={getUrl(bookmark.url)} alt="favicon" /> {bookmark.title}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</main>
--- static/background.js
+++ static/background.js
@@ -1,20 +1,36 @@
 const { tabs, runtime, action, contextMenus, bookmarks } = chrome
 
+const appName = 'oneTabSuper'
+const secretFolder = `${appName} (Do Not Touch!)`
+
 const openTabList = () => {
 	const fileName = 'index.html'
 	const url = runtime.getURL(fileName)
 	tabs.create({ url })
 }
 
+const findAppFolder = async () => {
+	const nodes = await bookmarks.search({ title: secretFolder })
+
+	if (nodes.length === 0) {
+		return (await bookmarks.create({ title: secretFolder, parentId: '2' })).id
+	}
+	return nodes[0].id
+}
+const appId = findAppFolder()
+
 const saveCurrentTab = async () => {
 	const { title, url, id } = (await tabs.query({ active: true, currentWindow: true }))[0]
+	const parentId = await findAppFolder()
 
-	const test1 = await bookmarks.create({ title, url })
+	console.log(await bookmarks.create({ title, url, parentId }))
 	tabs.remove(id)
 }
 
 // left click toolbar icon
 action.onClicked.addListener(saveCurrentTab)
+
+contextMenus.removeAll()
 
 // create context menu for right click toolbar icon
 contextMenus.create({
