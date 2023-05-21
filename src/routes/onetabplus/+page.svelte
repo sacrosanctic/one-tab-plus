456651af2b8ff3c80995c945224214478220362d
Scott Wu <sw@scottwu.ca>
2023-05-21 16:12:49 -0500

    make it a component

--- src/routes/onetabplus/+page.svelte
+++ src/routes/onetabplus/+page.svelte
@@ -1,14 +1,28 @@
 <script>
 	import { APP_NAME } from '$lib/constant'
 	import { getAppFolderId, isFolder } from '$lib/util'
-	import { andThen, filter, forEach, map, pipe, pluck } from 'ramda'
+	import {
+		andThen,
+		assoc,
+		converge,
+		filter,
+		forEach,
+		identity,
+		map,
+		pipe,
+		pluck,
+		prop,
+		reduce,
+		tap,
+	} from 'ramda'
 	import { onMount } from 'svelte'
-	import { allP } from 'ramda-adjunct'
+	import Tables from './Tables.svelte'
 
 	let worms = []
-	const setWorms = (data) => (worms = data)
+	const resetWorms = () => (worms = [])
+	const updateWorms = (data) => (worms = [...worms, data])
 
-	const removeBookmark = (id) => {
+	const removeBookmark = ({ id }) => {
 		chrome.bookmarks.remove(id)
 		loadBookmarks()
 	}
@@ -30,20 +44,27 @@
 
 	const loadBookmarks = () =>
 		pipe(
+			tap(resetWorms),
 			getAppFolderId,
 			andThen(chrome.bookmarks.getChildren),
 			andThen(
 				pipe(
 					//
 					filter(isFolder),
-					map(async (obj) => {
-						obj.children = await chrome.bookmarks.getChildren(obj.id)
-						return obj
+					forEach(async (folder) => {
+						folder.children = await chrome.bookmarks.getChildren(folder.id)
+						folder.children = map(
+							converge(
+								//
+								assoc('favicon'),
+								[pipe(prop('url'), getFavicon), identity],
+							),
+							folder.children,
+						)
+						updateWorms(folder)
 					}),
-					allP,
 				),
 			),
-			andThen(setWorms),
 		)()
 
 	const saveAllTabs = async () => {
@@ -94,20 +115,12 @@
 		chrome.tabs.remove(pluck('id', allTabs))
 	}
 
-	const formatDate = (date) =>
-		new Intl.DateTimeFormat('en-US', {
-			year: 'numeric',
-			month: 'numeric',
-			day: 'numeric',
-			hour: 'numeric',
-			minute: 'numeric',
-		}).format(new Date(date))
-
 	const addBookmarkListeners = () =>
 		forEach(
 			(event) => chrome.bookmarks[event].addListener(loadBookmarks),
 			['onCreated', 'onChanged', 'onMoved', 'onRemoved', 'onChildrenReordered'],
 		)
+	$: numOfTabs = reduce((acc, elm) => acc + elm.children.length, 0)(worms)
 
 	onMount(async () => {
 		loadBookmarks()
@@ -123,37 +136,11 @@
 	<button class="btn btn-primary" on:click={saveAllTabs}>get all tabs</button>
 	<button class="btn btn-primary" on:click={saveAllTabs2}>save all tabs (w/ folders)</button>
 	<h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
-		{APP_NAME} - {worms.length} tabs
+		{APP_NAME} - {numOfTabs} tabs
 	</h2>
-	{#each worms as { children, title }, i (i)}
-		<h2 class=" text-lg text-gray-900 dark:text-white mb-2 font-semibold">{title}</h2>
-		<table class="ml-2 space-y-1 text-gray-500 text-lg dark:text-gray-400 w-1/2">
-			<tbody>
-				{#each children as bookmark, i2 (i2)}
-					<!-- <pre>{JSON.stringify(bookmark, null, 2)}</pre> -->
-					<tr title={bookmark.url}>
-						<td class="whitespace-nowrap w-[0.1%]">
-							<button on:click={removeBookmark(bookmark.id)}>
-								<i class="fas fa-xmark fa-fw fa-lg text-gray-400" />
-							</button>
-						</td>
-						<td class="w-6"><img src={getFavicon(bookmark.url)} alt="favicon" /></td>
-						<td class="max-w-lg px-2">
-							<a
-								class="block text-ellipsis whitespace-nowrap overflow-hidden w-fit"
-								href={bookmark.url}
-								on:click|preventDefault={openBookmark(bookmark)}
-								target="_blank"
-							>
-								{bookmark.title}
-							</a>
-						</td>
-						<td class="text-gray-400 w-44">
-							{formatDate(bookmark.dateAdded)}
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	{/each}
+	<Tables
+		bind:bookmarks={worms}
+		on:openBookmark={(e) => openBookmark(e.detail)}
+		on:removeBookmark={(e) => removeBookmark(e.detail)}
+	/>
 </main>
