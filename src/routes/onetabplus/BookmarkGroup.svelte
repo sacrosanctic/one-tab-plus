c76950ff65af6bcbab9b838c2246d0484d58dcb6
Scott Wu <sw@scottwu.ca>
2023-06-08 01:17:43 -0700

    add drag handle

--- src/routes/onetabplus/BookmarkGroup.svelte
+++ src/routes/onetabplus/BookmarkGroup.svelte
@@ -1,20 +1,47 @@
 <script>
 	import InPlaceInput from './InPlaceInput.svelte'
 	import { fade } from 'svelte/transition'
-	import { dndzone } from 'svelte-dnd-action'
+	import { dndzone, SOURCES, TRIGGERS } from 'svelte-dnd-action'
 	import { isEmpty, isNil } from 'ramda'
 	import { createEventDispatcher } from 'svelte'
+	import { flip } from 'svelte/animate'
 
 	const dispatch = createEventDispatcher()
 
 	export let bookmarks = {}
+	let dragDisabled = true
 
-	const flipDurationMs = 300
+	const flipDurationMs = 200
 	const handleDndConsider = (e) => {
 		bookmarks.children = e.detail.items
+
+		// Ensure dragging is stopped on drag finish via keyboard
+		if (
+			e.detail.info.source === SOURCES.KEYBOARD &&
+			e.detail.info.trigger === TRIGGERS.DRAG_STOPPED
+		)
+			dragDisabled = true
 	}
+
 	const handleDndFinalize = (e) => {
 		bookmarks.children = e.detail.items
+		// Ensure dragging is stopped on drag finish via pointer (mouse, touch)
+		if (e.detail.info.source === SOURCES.POINTER) dragDisabled = true
+	}
+
+	const startDrag = (e) => {
+		// preventing default to prevent lag on touch devices (because of the browser checking for screen scrolling)
+		e.preventDefault()
+		dragDisabled = false
+	}
+
+	const stopDrag = (e) => {
+		e.preventDefault()
+		dragDisabled = true
+	}
+
+	const handleKeyDown = (e) => {
+		if ((e.key === 'Enter' || e.key === ' ') && dragDisabled) dragDisabled = false
 	}
 
 	const formatDate = (date) =>
@@ -34,7 +61,7 @@
 	</h2>
 	<section class="text-gray-500 text-lg dark:text-gray-400 w-full sm:w-4/6">
 		<ul
-			use:dndzone={{ items: bookmarks.children, flipDurationMs }}
+			use:dndzone={{ items: bookmarks.children, dragDisabled, flipDurationMs }}
 			on:consider={handleDndConsider}
 			on:finalize={handleDndFinalize}
 			class="space-y-4"
@@ -42,45 +69,57 @@
 			{#each bookmarks.children as bookmark (bookmark.id)}
 				<!-- <Bookmark {bookmark} on:openBookmark on:removeBookmark />
 				 -->
-				{#if isNil(bookmark) || isEmpty(bookmark)}
-					<li
-						class="flex items-center gap-2 rounded-md p-2 bg-white dark:bg-inherit dark:border-gray-300 dark:border"
-					>
-						<div class="ml-1 w-4" />
-						<i class="fas fa-xmark h-11 fa-3x aspect-square text-red-200" />
-						<div
-							class="text-gray-900 dark:text-gray-300 block whitespace-nowrap overflow-hidden text-ellipsis w-full max-w-lg capitalize font-medium leading-none"
+				<div animate:flip={{ duration: flipDurationMs }} class="cursor-default">
+					{#if isNil(bookmark) || isEmpty(bookmark)}
+						<li
+							class="flex items-center gap-2 rounded-md p-2 bg-white dark:bg-inherit dark:border-gray-300 dark:border"
 						>
-							invalid bookmark
-						</div>
-					</li>
-				{:else}
-					<li
-						title={bookmark.url}
-						class="flex items-center gap-2 rounded-md p-2 bg-white dark:border-gray-300 dark:bg-inherit dark:border"
-					>
-						<button class="w-4 opacity-0" on:click={dispatch('removeBookmark', bookmark)}>
-							<i class="fas fa-xmark fa-lg text-gray-400" />
-						</button>
-						<button aria-label="drag-handle">
-							<img class="h-9 aspect-square" src={bookmark.favicon} alt="favicon" />
-						</button>
-
-						<div class="overflow-hidden">
-							<a
+							<div class="ml-1 w-4" />
+							<i class="fas fa-xmark h-11 fa-3x aspect-square text-red-200" />
+							<div
 								class="text-gray-900 dark:text-gray-300 block whitespace-nowrap overflow-hidden text-ellipsis w-full max-w-lg capitalize font-medium leading-none"
-								href={bookmark.url}
-								on:click|preventDefault={dispatch('openBookmark', bookmark)}
 							>
-								{bookmark.title}
-							</a>
-							<p class="font-medium text-xs mt-1">
-								<i class="far fa-calendar mr-1" />
-								{formatDate(bookmark.dateAdded)}
-							</p>
-						</div>
-					</li>
-				{/if}
+								invalid bookmark
+							</div>
+						</li>
+					{:else}
+						<li
+							title={bookmark.url}
+							class="flex items-center gap-2 rounded-md p-2 bg-white dark:border-gray-300 dark:bg-inherit dark:border"
+						>
+							<button class="w-4 opacity-0" on:click={dispatch('removeBookmark', bookmark)}>
+								<i class="fas fa-xmark fa-lg text-gray-400" />
+							</button>
+							<button
+								class="h-9 aspect-square"
+								tabindex={dragDisabled ? 0 : -1}
+								aria-label="drag-handle"
+								class:cursor-grab={dragDisabled}
+								class:cursor-grabbing={!dragDisabled}
+								on:mousedown={startDrag}
+								on:mouseup={stopDrag}
+								on:touchstart={startDrag}
+								on:keydown={handleKeyDown}
+							>
+								<img class="w-full" src={bookmark.favicon} alt="favicon" />
+							</button>
+
+							<div class="overflow-hidden">
+								<a
+									class="text-gray-900 dark:text-gray-300 block whitespace-nowrap overflow-hidden text-ellipsis w-full max-w-lg capitalize font-medium leading-none"
+									href={bookmark.url}
+									on:click|preventDefault={dispatch('openBookmark', bookmark)}
+								>
+									{bookmark.title}
+								</a>
+								<p class="font-medium text-xs mt-1">
+									<i class="far fa-calendar mr-1" />
+									{formatDate(bookmark.dateAdded)}
+								</p>
+							</div>
+						</li>
+					{/if}
+				</div>
 			{/each}
 		</ul>
 	</section>
